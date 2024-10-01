bd6897f8d3e7947aaeec61a8847b04a61f98e2bb
lanznx <brandonlu0924@gmail.com>
2024-10-02 01:26:52 +0800

    remove: payment event

--- product-order/src/main/java/com/example/productorder/MQ/PaymentFailedEvent.java
+++
@@ -1,9 +0,0 @@
-package com.example.productorder.MQ;
-
-import java.util.UUID;
-
-public class PaymentFailedEvent extends PaymentEvent {
-    public PaymentFailedEvent(UUID orderId) {
-        super(orderId);
-    }
-}
--- product-order/src/main/java/com/example/productorder/MQ/PaymentSuccessfulEvent.java
+++
@@ -1,9 +0,0 @@
-package com.example.productorder.MQ;
-
-import java.util.UUID;
-
-public class PaymentSuccessfulEvent extends PaymentEvent {
-    public PaymentSuccessfulEvent(UUID orderId) {
-        super(orderId);
-    }
-}
