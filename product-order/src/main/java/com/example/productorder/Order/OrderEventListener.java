1faf2cca90851ecebd1c89864d2b3c929c44a629
lanznx <brandonlu0924@gmail.com>
2024-10-01 19:23:46 +0800

    refactor: order event listener

--- product-order/src/main/java/com/example/productorder/Order/OrderEventListener.java
+++ product-order/src/main/java/com/example/productorder/Order/OrderEventListener.java
@@ -1,13 +1,13 @@
 package com.example.productorder.Order;
 
-import com.example.productorder.MQ.PaymentFailedEvent;
-import com.example.productorder.MQ.PaymentSuccessfulEvent;
-import com.example.productorder.Order.entity.Order;
+ import com.example.productorder.Order.entity.Order;
 
 import com.example.productorder.Order.entity.OrderStatus;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
+
+import java.util.UUID;
 
 @Component
 public class OrderEventListener {
@@ -16,15 +16,15 @@ public class OrderEventListener {
     private OrderService orderService;
 
     @KafkaListener(topics = "payment-successful")
-    public void handlePaymentSuccessfulEvent(PaymentSuccessfulEvent event) {
-        orderService.updateOrderStatus(event.getOrderId(), OrderStatus.PAID);
+    public void handlePaymentSuccessfulEvent(String orderId) {
+        orderService.updateOrderStatus(UUID.fromString(orderId), OrderStatus.PAID);
     }
 
     @KafkaListener(topics = "payment-failed")
-    public void handlePaymentFailedEvent(PaymentFailedEvent event) {
-        orderService.updateOrderStatus(event.getOrderId(), OrderStatus.CANCELED);
+    public void handlePaymentFailedEvent(String orderId) {
+        orderService.updateOrderStatus(UUID.fromString(orderId), OrderStatus.CANCELED);
 
-        Order order = orderService.getOrderById(event.getOrderId());
+        Order order = orderService.getOrderById(UUID.fromString(orderId));
         orderService.releaseStock(order.getProductId(), order.getQuantity());
     }
 }
